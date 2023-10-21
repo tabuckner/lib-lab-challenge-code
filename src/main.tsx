ad4ed67eb18d7ca043970ff9f7d23cb759b6d0e6
Taylor Buckner <taylor.a.buckner@gmail.com>
2023-10-21 15:49:14 -0500

    [scaffold] - Implement `appQueryClient` and `BrowserRouter`.

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,19 @@ import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 
 import App from './app/app';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { appQueryClient } from './client';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={appQueryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>
 );
